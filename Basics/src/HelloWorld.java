f04185cd78867730184c2c2ed56f4cc4d52a2f7b
Yumnah Jaleel <gq4101@myamu.ac.in>
2026-01-31 22:38:19 +0530

    Add files via upload

---
+++ Basics/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My name is Yumnah\nWelcome to Sturdy-Waffle!");
+    }
+}
