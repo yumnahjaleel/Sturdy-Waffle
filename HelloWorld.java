2c7434f344639ea13cd5fa9f601d735eaaa614d3
Yumnah Jaleel <gq4101@myamu.ac.in>
2026-02-02 17:43:14 +0530

    Add comment in HelloWorld main method
    Added a comment to the main method.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("My name is Yumnah\nWelcome to Sturdy-Waffle!");
+        //this is a comment
     }
 }
+
